cec3c6bbb7082396169a254c5e7082d7461b6aaa
JimeRosello <jimena.rosello@gmail.com>
2019-04-11 01:23:31 -0300

    Add trend and average methods, correct code style

--- TP1Math&Statistics/src/com/tp1/MathFunctions.java
+++ TP1Math&Statistics/src/com/tp1/MathFunctions.java
@@ -1,25 +1,29 @@
 package com.tp1;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class MathFunctions {
 
-	public static int Max(int[] vector, boolean ordered) {
+	public static int max(int[] vector, boolean ordered) {
 		int max = 0;
 		if (ordered) {
 			max = vector[0];
 		} else {
-			for (int i = 0; i < vector.length; i++) {
-				if (vector[i] > max) {
-					max = vector[i];
+			for (int number : vector) {
+				if (number > max) {
+					max = number;
 				}
 			}
 		}
 		return max;
 	}
 
-	public static int Max(List<Integer> list) {
+	public static int max(List<Integer> list) {
 		Iterator iterator = list.iterator();
 		int max = 0;
 		while (iterator.hasNext()) {
@@ -29,6 +33,65 @@ public class MathFunctions {
 			}
 		}
 		return max;
+	}
+
+	public static double average(int[] vector) {
+		double sum = 0;
+		for (int number : vector) {
+			sum += number;
+		}
+		return sum / vector.length;
+	}
+
+	public static double average(List<Integer> list) {
+		Iterator iterator = list.iterator();
+		double sum = 0;
+		while (iterator.hasNext()) {
+			sum += (Integer) iterator.next();
+		}
+		return sum / list.size();
+	}
+
+	public static int trend(int[] vector) {
+		Map<Integer, Integer> occurrences = new LinkedHashMap<>();
+		for (int number : vector) {
+			int previousOccurrences = occurrences.get(number);
+			occurrences.put(number, ++previousOccurrences);
+		}
+		int maxOccurrences = 0;
+		int trend = 0;
+		Set entrySet = occurrences.entrySet();
+		Iterator<Map.Entry<Integer, Integer>> iterator = entrySet.iterator();
+		int size = entrySet.size();
+		while (iterator.hasNext() && maxOccurrences < size / 2) {
+			Entry<Integer, Integer> entry = iterator.next();
+			if (entry.getValue() > maxOccurrences) {
+				trend = entry.getKey();
+				maxOccurrences = entry.getValue();
+			}
+		}
+		return trend;
+	}
+
+	public static int trend(List<Integer> list) {
+		Map<Integer, Integer> occurrences = new LinkedHashMap<>();
+		for (int number : list) {
+			int previousOccurrences = occurrences.get(number);
+			occurrences.put(number, ++previousOccurrences);
+		}
+		int maxOccurrences = 0;
+		int trend = 0;
+		Set entrySet = occurrences.entrySet();
+		Iterator<Map.Entry<Integer, Integer>> iterator = entrySet.iterator();
+		int size = entrySet.size();
+		while (iterator.hasNext() && maxOccurrences < size / 2) {
+			Entry<Integer, Integer> entry = iterator.next();
+			if (entry.getValue() > maxOccurrences) {
+				trend = entry.getKey();
+				maxOccurrences = entry.getValue();
+			}
+		}
+		return trend;
 	}
 
 }
